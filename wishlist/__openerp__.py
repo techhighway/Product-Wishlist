307846fc293c14417352f86b619aa42ea9d48182
techhighway <adityab@techhighway.co.in>
2015-11-23 19:38:43 +0530

    to change image

--- wishlist/__openerp__.py
+++ wishlist/__openerp__.py
@@ -13,10 +13,10 @@ OpenERP E-Commerce
 	'depends'	: ['website_sale'],
     	'data'	: [
 		'views/template.xml',
-        'views/wishlist_template_view.xml',
+        	'views/wishlist_template_view.xml',
     		],
     	'qweb'	: ['static/src/xml/*.xml'],
-	'images': ['static/description/wishlist_home.png.png'], 
+	'images': ['static/description/wishlist_home.png'],	
     	'installable': True,
     	'application': True,
 }
